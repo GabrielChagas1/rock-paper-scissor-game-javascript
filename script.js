7ca0549eca64ac2bea61d9ea60337d2b0e55154c
Gabriel Chagas <gabrielserq@outlook.com>
2022-09-29 17:33:07 -0300

    chore: creating element comp_choice.

--- script.js
+++ script.js
@@ -21,3 +21,6 @@ let choices_object = {
 
     var choices = ["rock", "paper", "scissor"];
     var num = Math.floor(Math.random()*3);
+
+    document.getElementById("comp_choice").innerHTML = 
+    ` Computer choose <span> ${choices[num].toUpperCase()} </span>`;
