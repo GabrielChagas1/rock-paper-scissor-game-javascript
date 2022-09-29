c5474aa39a20761da7217fe268fb8d7869954803
Gabriel Chagas <gabrielserq@outlook.com>
2022-09-29 17:34:27 -0300

    chore: creating element user_choice.

--- script.js
+++ script.js
@@ -24,3 +24,6 @@ let choices_object = {
 
     document.getElementById("comp_choice").innerHTML = 
     ` Computer choose <span> ${choices[num].toUpperCase()} </span>`;
+
+    document.getElementById("user_choice").innerHTML = 
+    ` You choose <span> ${input.toUpperCase()} </span>`;
