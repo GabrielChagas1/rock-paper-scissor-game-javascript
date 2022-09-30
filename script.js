1aaf05aa56c48218248c9f3735152f7b0e29337d
Gabriel Chagas <gabrielserq@outlook.com>
2022-09-30 10:05:57 -0300

    chore: creating switch choices_object

--- script.js
+++ script.js
@@ -29,3 +29,20 @@ let choices_object = {
     ` You choose <span> ${input.toUpperCase()} </span>`;
 
     let computer_choice = choices[num];
+
+    switch(choices_object[input][computer_choice]){
+        case 'win':
+            result_ref.style.cssText = "background-color: #cefdce; color: #689f38";
+            result_ref.innerHTML = "YOU WIN";
+            user_score++;
+            break;
+        case 'lose':
+            result_ref.style.cssText = "background-color: #ffdde0; color: #d32f2f";
+            result_ref.innerHTML = "YOU LOSE";
+            computer_score++;
+            break;
+        default:
+            result_ref.style.cssText = "background-color: #e5e5e5; color: #808080";
+            result_ref.innerHTML = "DRAW";
+            break;
+    }
